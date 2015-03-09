7cdb7a39966a9d76c34acd98eacfc73eb26bd4eb
alex vasquez <alex@digisavvy.com>
2015-03-08 17:09:43 -0700

    Moved buttons to a function, did same with post nav function

--- single.php
+++ single.php
@@ -13,10 +13,8 @@ get_header(); ?>
     <?php while ( have_posts() ) : the_post(); ?>
 
     <?php get_template_part( 'page-templates/partials/content', 'single' ); ?>
-	<?php add_rrssb_buttons(); ?>
-	<?php echo get_the_post_navigation(); ?>
 
-    <?php
+	<?php
 				// If comments are open or we have at least one comment, load up the comment template
 	if ( comments_open() || '0' != get_comments_number() ) :
 		comments_template();
